6fb4348695b63664d33462ade5b75d267a2830b4
MikeyBurkman <michael.burkman@gmail.com>
2018-11-04 08:30:03 -0500

    Prefix trivia answers with letter names

---
+++ src/robot.js
@@ -0,0 +1,29 @@
+
+const trivia = require('./trivia')
+
+module.exports = robot => {
+
+  // TODO
+  // robot.hear(/^!triviatopics$/i, res => {
+  //   res.send
+  // })
+
+  robot.hear(/^!trivianew ?(.*)$/i, res => {
+    trivia
+        .newQuestion()
+        .then(s => res.send(s))
+  })
+
+  robot.hear(/^!triviarepeat$/i, res => {
+    trivia
+        .repeatQuestion()
+        .then(s = res.send(s))
+  })
+
+  robot.hear(/^!ans ([abcdABCD])$/i, res => {
+    trivia
+        .answer(res.match[1])
+        .then(s = res.send(s))
+  })
+
+}
