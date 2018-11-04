788f15a9ccdc4ae1a382df76df03276ce10c69ef
MikeyBurkman <michael.burkman@gmail.com>
2018-11-04 08:41:32 -0500

    Refactor

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const path = require('path')
+const trivia = require('.src/hubot-simple-trivia')
 
-module.exports = (robot) => {
-  const scriptsPath = path.resolve(__dirname, 'src')
-  robot.loadFile(scriptsPath, 'robot.js')
+module.exports = robot => {
+
+  // TODO
+  // robot.hear(/^!triviatopics$/i, res => {
+  //   res.send
+  // })
+
+  robot.hear(/^!trivianew ?(.*)$/i, res => {
+    trivia
+        .newQuestion()
+        .then(s => res.send(s))
+  })
+
+  robot.hear(/^!triviarepeat$/i, res => {
+    trivia
+        .repeatQuestion()
+        .then(s = res.send(s))
+  })
+
+  robot.hear(/^!ans ([abcdABCD])$/i, res => {
+    trivia
+        .answer(res.match[1])
+        .then(s = res.send(s))
+  })
+
 }
